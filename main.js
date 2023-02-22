9845a5370eae05c901aee72bc1f107e4e2f576bf
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-02-22 12:02:26 +0100

    ak

--- main.js
+++ main.js
@@ -4,83 +4,87 @@ let imgSection = document.querySelector('#img')
 let pageNumber = 1;
 let input;
 let color;
+let totalHits;
+let imgrespons;
 
 imgSerchButton.onsubmit = async event => {
    event.preventDefault();
-
-   imgSection.innerHTML = '';
-
-   let imgCount = 0;
+   removeElement()
 
    input = imgSerchButton.input.value;
    color = imgSerchButton.colors.value;
    let url = 'https://pixabay.com/api/?key=33474684-1ead600a61d07e2bdfb2e093e&q=' + input + '&colors=' + color + '&per_page=10'
-
    let response = await fetch(url);
    let json = await response.json();
+   totalHits = json.totalHits;
+   document.getElementById("nextPageButton").checked = true;
 
-   console.log(json);
+   GetImages(url);
+   ButtonVisibililty();
+   // if (pageNumber != 1) {
+   //    document.getElementById('previousPageButton').style.visibility = 'visible';
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
+   // else {
+   //    document.getElementById('previousPageButton').style.visibility = 'hidden';
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
+}
 
-   if (pageNumber = 1) {
+function ButtonVisibililty(){
+   if(pageNumber == 1){
       document.getElementById('previousPageButton').style.visibility = 'hidden';
       document.getElementById('nextPageButton').style.visibility = 'visible';
    }
-
-   for (let imgrespons of json.hits) {
-      var imgelement = document.createElement('img');
-      var divelemnt = document.createElement('div');
-      var p = document.createElement('p');
-      var pTags = document.createElement('p');
-
-      divelemnt.classList.add('imgcontainer');
-      imgelement.classList.add('styleimg')
-
-      imgelement.src = imgrespons.webformatURL;
-      p.innerText = imgrespons.user;
-      pTags.innerText = imgrespons.tags
-
-      divelemnt.appendChild(imgelement);
-      divelemnt.appendChild(p);
-      divelemnt.appendChild(pTags);
-      imgSection.appendChild(divelemnt);
-
-      imgCount++;
-
-     
-   }
-}
-
-function previousPageSelector() {
-   pageNumber--
-   if (pageNumber != 1) {
+   else if(pageNumber != 1 && totalHits > 10){
       document.getElementById('previousPageButton').style.visibility = 'visible';
       document.getElementById('nextPageButton').style.visibility = 'visible';
    }
-   else {
-      document.getElementById('previousPageButton').style.visibility = 'hidden';
-      document.getElementById('nextPageButton').style.visibility = 'visible';
+   else if(totalHits < 10){
+      document.getElementById('previousPageButton').style.visibility = 'visible';
+      document.getElementById('nextPageButton').style.visibility = 'hidden';
    }
+   // if (pageNumber != 1) {
+   //    document.getElementById('previousPageButton').style.visibility = 'visible';
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
+   // else {
+   //    document.getElementById('previousPageButton').style.visibility = 'hidden';
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
+}
+
+function previousPageSelector(){
+   pageNumber--
+   // ButtonVisibililty();
+   // if (pageNumber != 1) {
+   //    document.getElementById('previousPageButton').style.visibility = 'visible';
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
+   
+   document.getElementById("previousPageButton").checked = true;
+   document.getElementById("nextPageButton").checked = false;
    pageSelector()
+//    ButtonVisibililty();
 }
 
 function nextPageSelector() {
    pageNumber++
-   if (pageNumber != 1) {
-      document.getElementById('previousPageButton').style.visibility = 'visible';
-   }
+   // ButtonVisibililty();
+   // if (pageNumber != 1) {
+   //    document.getElementById('previousPageButton').style.visibility = 'visible';
+   // }
+   document.getElementById("previousPageButton").checked = false;
+   document.getElementById("nextPageButton").checked = true;
    pageSelector()
+   // ButtonVisibililty();
 }
 
-async function pageSelector() {
-   event.preventDefault();
-   removeElement();
-
-   imgCount = 0;
-
-   let url = 'https://pixabay.com/api/?key=33474684-1ead600a61d07e2bdfb2e093e&q=' + input + '&colors=' + color + '&page=' + pageNumber + '&per_page=10'
-
+async function GetImages(url){
    let response = await fetch(url);
    let json = await response.json();
+   let checkPrevious = document.getElementById('previousPageButton').checked;
+   let checkNext = document.getElementById('nextPageButton').checked;
 
    console.log(json);
 
@@ -101,14 +105,30 @@ async function pageSelector() {
       divelemnt.appendChild(p);
       divelemnt.appendChild(pTags);
       imgSection.appendChild(divelemnt);
-
-      imgCount++;
-
-     
+      if(checkPrevious == true){
+         totalHits++
+      }
+      else if(checkNext == true){
+         totalHits--
+      }
    }
-   if (imgCount < 10) {
-      document.getElementById('nextPageButton').style.visibility = 'hidden';
-   }
+}
+
+async function pageSelector() {
+   event.preventDefault();
+   removeElement();
+
+   let url = 'https://pixabay.com/api/?key=33474684-1ead600a61d07e2bdfb2e093e&q=' + input + '&colors=' + color + '&page=' + pageNumber + '&per_page=10'
+
+   GetImages(url);
+
+   ButtonVisibililty();
+   // if (totalHits <= 10) {
+   //    document.getElementById('nextPageButton').style.visibility = 'hidden';
+   // }
+   // else{
+   //    document.getElementById('nextPageButton').style.visibility = 'visible';
+   // }
 }
 
 function removeElement() {
